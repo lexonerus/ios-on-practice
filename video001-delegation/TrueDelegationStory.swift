dbce291dfe52550de190508adb25def2d105ce32
Alexey Krzywicki <lexone01@gmail.com>
2021-09-30 19:57:47 +0300

    Update TrueDelegationStory.swift

--- video001-delegation/TrueDelegationStory.swift
+++ video001-delegation/TrueDelegationStory.swift
@@ -9,54 +9,54 @@
 // A protocol is like a list of rules that need to be followed
 // Протокол можно представить, как список правил, которым необходимо следовать
 
-protocol OlderGremlinDelegate: AnyObject {
-    func getAGlassOfWaterToOlderGremlin()
-    // This protocol named OlderGremlinDelegate has function, but realization
+protocol OlderHobbitDelegate: AnyObject {
+    func getAGlassOfWaterToOlderHobbit()
+    // This protocol named OlderHobbitDelegate has function, but realization
     // of this function is not important for now
-    // Данный протокол с именем OlderGremlinDelegate имеет функцию, 
+    // Данный протокол с именем OlderHobbitDelegate имеет функцию, 
     // но реализация данной функции совсем не важна для нас сейчас
 }
 
 // MARK: Characters in the story / Персонажи нашей истории
 
-// Old gremlin (master)
-// Старый гремлин (мастер)
-class GrandfatherGremlin {
-    // Can make whichever other gremlin is around at
+// Old hobbit (master)
+// Старый хоббит (мастер)
+class GrandfatherHobbit {
+    // Can make whichever other hobbit is around at
     // the time be hes delegate (slave)
-    // Способен дать задание любому другому гремлину
+    // Способен дать задание любому другому хоббитy
     // кто младше него (раб)
-    weak var delegate: OlderGremlinDelegate?
+    weak var delegate: OlderHobbitDelegate?
     
-    func tellAnyGremlinToGetMeSomeWater() {
+    func tellAnyHobbitToGetMeSomeWater() {
         // The delegate is optional because even through
         // Im thirsty, there might not be anyone nearby
         // that I can boss around
         // Делегирование опционально, тк вокруг может
-        // не быть других гремлинов способных выполнить задание
-        delegate?.getAGlassOfWaterToOlderGremlin()
+        // не быть других хоббитов способных выполнить задание
+        delegate?.getAGlassOfWaterToOlderHobbit()
     }
 }
 
-// Grandchildren of Old Gremlin
+// Grandchildren of Old Hobbit
 // Внуки старого гремлина
-class LittleGremlinBrothers: OlderGremlinDelegate {
-    func getAGlassOfWaterToOlderGremlin() {
-        // Little gremlins follows the letter of the law (protocol),
+class BadGrandson: OlderHobbitDelegate {
+    func getAGlassOfWaterToOlderHobbit() {
+        // Little hobbits follows the letter of the law (protocol),
         // but no one said exactly how they had to respond
-        // Маленькие гремлинские братья обяханы следовать букве закона (протоколу),
+        // Маленькие хоббиты обяханы следовать букве закона (протоколу),
         // но никто не говорил, как именно они должны выполнять эту функцию
         print("Go get it yourself, you old faggot!")
         // Послали деда, пусть сам идет себе за водой
     }
 }
 
-// Gremlin sister
+// Hobbit granddaoughter
 // also follows protocol
-// Сестра старого гремлина
+// Внучка старого хоббитa
 // так же следует протоколу
-class GremlinSister: OlderGremlinDelegate {
-    func getAGlassOfWaterToOlderGremlin() {
+class GoodGranddaughter: OlderHobbitDelegate {
+    func getAGlassOfWaterToOlderHobbit() {
         // Another response
         // Другая реакция
         print("Here is your water")
@@ -65,41 +65,41 @@ class GremlinSister: OlderGremlinDelegate {
 
 // MARK: The Story / История
 
-// Older Gremlin Grandfather laying on the couch and watching hockey on TV
-// Старый дед Гремлин лежит на диване и смотрит хоккейный матч по ТВ
-let oldGremlin = GrandfatherGremlin()
+// Older Hobbit Grandfather laying on the couch and watching hockey on TV
+// Старый дед хоббит лежит на диване и смотрит хоккейный матч по ТВ
+let oldHobbit = GrandfatherHobbit()
 
-// He has a little grandson named Mark
-// У него есть внук по имени Марк
-let mark = LittleGremlinBrothers()
-// Also he has Sally, his sister
-// Так же у него есть младшая сетстра Салли
-let sally = GremlinSister()
+// He has a little grandson named Ivan
+// У него есть внук по имени Иван
+let ivan = BadGrandson()
+// Also he has Masha, his granddouughter
+// Так же у него есть внучка Маша
+let masha = GoodGranddaughter()
 
 
-// Mark walks into the room. Now old Gremlin has someone to ask
-// Марк заходит в комнату, теперь у Деду есть к кому обратиться
-oldGremlin.delegate = mark
+// Ivan walks into the room. Now old Hobbit has someone to ask
+// Иван заходит в комнату, теперь у Деду есть к кому обратиться
+oldHobbit.delegate = ivan
 
-// So he tells to Mark to get him some water
-// Поэтому он просит Марка принести ему стакан воды
-oldGremlin.tellAnyGremlinToGetMeSomeWater()
+// So he tells to Ivan to get him some water
+// Поэтому он просит Ивана принести ему стакан воды
+oldHobbit.tellAnyHobbitToGetMeSomeWater()
 
 // Unfortunately no one lived happily ever after...
-// And Mark go away
-// Но Марк рявкнул на деда, и ушел
+// And Ivan go away
+// Но Иван рявкнул на деда, и ушел
 
-// Few hours later Sally came into the old Gremlin room
-// Old Gremlin can now ask her
-// Пару часов спустя в комнату к Деду заходит младшая сестра
+// Few hours later Masha came into the old Hobbit room
+// Old Hobbit can now ask her
+// Пару часов спустя в комнату к Деду заходит Маша
 // Теперь можно взаимодействать с ней
-oldGremlin.delegate = sally
+oldHobbit.delegate = masha
 
-// And now Old Gremlin tells Sally that she get him some water
-// Старый Гремлин попросил у Салли воды
-oldGremlin.tellAnyGremlinToGetMeSomeWater()
-// Sally love and respect older Gremlin, and bring him a galss of water
-// Салли любит своего старшего брата, поэтому пошла и принесла ему воды
+// And now Old Hobbit tells Masha that she get him some water
+// Старый хоббит попросил у Маши воды
+oldHobbit.tellAnyHobbitToGetMeSomeWater()
+// Masha love and respect older Hobbit, and bring him a galss of water
+// Маша любит своего деда, поэтому пошла и принесла ему воды
 
 // Happy end.
 // Счастливый финал.
