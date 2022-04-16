5bc795cee9ec946d7e5a1d6e54de84c30c624351
Alexey Krzywicki <lexone01@gmail.com>
2022-04-16 19:55:26 +0300

    Video 52
    Unwrapping Optionals playground creation

---
+++ patterns-and-architectures/v052-UnwrappingOptionals/UnwrapOptional.playground/Contents.swift
@@ -0,0 +1,37 @@
+var text: String? = "some text"
+//var text: String? = nil
+
+// 1. Пинудительно (forced) - не безопасно
+//var unwrapped1: String = text! // "unexpectedly found nil while unwrapping an Optional value"
+
+// 2. Неявно (implicitly) - не безопасно
+//var unwrapped2 = text! // "Execution was interrupted"
+
+// 3. Опциональное привязывание (binding) - безопасно
+if let unwrapped3 = text {
+    print("optional was successfully unwrapped and its value = \(unwrapped3)")
+} else {
+    print("found nil in optional value")
+}
+
+// 4. Опциональная цепочка (chaining) - безопасно
+let unwrapped4 = text?.uppercased()
+print(unwrapped4 as Any)
+
+// 5. Значение по умолчанию - безопасно
+let unwrapped5 = text ?? "default value"
+print(unwrapped5 as Any)
+
+// 6. С помощью Guard - безопасно
+func guardTest() {
+    guard let unwrapped6 = text else {
+        return
+    }
+    print(unwrapped6 as Any)
+}
+guardTest()
+
+// 7. Optional паттерн - безопасно
+if case let unwrapped7? = text {
+    print(unwrapped7)
+}
